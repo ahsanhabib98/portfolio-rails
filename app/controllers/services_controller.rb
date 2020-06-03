3f5fea5823bed56b8bff9519b7caec601e418cdd
Ahsan Habib <ahredoan@gmail.com>
2020-06-04 00:30:19 +0600

    implement the ability to edit database

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -18,4 +18,20 @@ class ServicesController < ApplicationController
       end
     end
   end
+
+  def edit
+    @service_item = Service.find(params[:id])
+  end
+
+  def update
+    @service_item = Service.find(params[:id])
+    
+    respond_to do |format|
+      if @service_item.update(params.require(:service).permit(:title, :subtitle, :body))
+        format.html { redirect_to services_path, notice: 'Service item was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
 end
