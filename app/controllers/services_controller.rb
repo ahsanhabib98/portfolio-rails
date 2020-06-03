4f2e0718da5258d6dfcc5e0f162f8e34b0a83dd7
Ahsan Habib <ahredoan@gmail.com>
2020-06-03 23:04:27 +0600

    build the new and create functionality

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -2,4 +2,20 @@ class ServicesController < ApplicationController
   def index
     @service_items = Service.all
   end
+
+  def new
+    @service_item = Service.new
+  end
+
+  def create
+    @service_item = Service.new(params.require(:service).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @service_item.save
+        format.html { redirect_to services_path, notice: 'Service item was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
 end
