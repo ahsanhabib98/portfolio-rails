bdbc71bfda2cff756444ea44344bfe59a017b2d9
Ahsan Habib <ahredoan@gmail.com>
2020-06-05 03:38:49 +0600

    implement custom routes for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :services, except: [:show]
   get 'service/:id', to: 'services#show', as: 'service_show'
 
