b026c31492f5563e36ec8e68d3ab635790866f72
ahsanhabib98 <ahredoan@gmail.com>
2020-09-16 00:08:45 +0600

    add alert notificaton used shared view helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -58,6 +58,18 @@ module ApplicationHelper
   def active? path
     "active" if current_page? path
   end
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Ahsan Habib Portfolio", sticky: false)
+  end
 end
 
 # Copyright generator
